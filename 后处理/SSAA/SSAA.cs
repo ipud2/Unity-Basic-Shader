c972a01035e189ddd1c99ab9ef8358b677011b8c
ipud2 <ipud2@users.noreply.github.com>
2021-07-23 19:29:09 +0800

    SSAA 最牛逼 最暴力 最耗费 的抗锯齿算法

---
+++ 后处理/SSAA/SSAA.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+namespace TA
+{
+    public class SSAA : MonoBehaviour
+    {
+
+        public enum Level
+        {
+            x1 = 1,
+            x2 = 2,
+            x3 = 3,
+            x4 = 4,
+            x5 = 5,
+            x6 = 6,
+            x7 = 7,
+            x8 = 8,
+        };
+        
+
+        public Level level = Level.x3;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            // Init();
+        }
+
+        private Material mat = null;
+        private RenderTexture SSAART = null;
+        // private Camera camera = null;
+
+        public void SetRT()
+        {
+            float scale = (int)level;
+            var graphicsFormat = SystemInfo.GetCompatibleFormat(GraphicsFormatUtility.GetGraphicsFormat(RenderTextureFormat.Default, false), FormatUsage.MSAA8x);
+            var depthRTDesc = new RenderTextureDescriptor((int)(Screen.width*3), (int)(Screen.height*3), graphicsFormat,32);
+            depthRTDesc.sRGB = false;
+            depthRTDesc.memoryless = RenderTextureMemoryless.None;
+            depthRTDesc.msaaSamples = 8;
+            depthRTDesc.autoGenerateMips = false;
+            depthRTDesc.useMipMap = false;
+            
+            QualitySettings.antiAliasing = 8;
+            QualitySettings.shadowResolution = ShadowResolution.VeryHigh;
+            // SSAART =  new RenderTexture(Screen.width*scale, Screen.height*scale,-1);
+            SSAART =  new RenderTexture(depthRTDesc);
+            cam.targetTexture = SSAART;
+            cam.allowMSAA = true;
+        }
+
+        private Camera cam = null;
+        public void Init()
+        {
+            Camera HeroCam = GetComponent<Camera>();
+            // GameObject newCam = new GameObject("newCam");
+            GameObject newCam = Instantiate<GameObject>(gameObject);
+            if (newCam.GetComponent<SSAA>())
+            {
+                Destroy(newCam.GetComponent<SSAA>());
+            }
+            newCam.transform.position = gameObject.transform.position;
+            newCam.transform.rotation = gameObject.transform.rotation;
+            newCam.transform.localScale = gameObject.transform.localScale;
+            // Camera cam = newCam.AddComponent<Camera>();
+            cam = newCam.GetComponent<Camera>();
+            cam.CopyFrom(HeroCam);
+            SetRT();
+            Shader.SetGlobalTexture("SSAART",SSAART);
+        }
+
+        private void OnRenderImage(RenderTexture src, RenderTexture dest)
+        {
+            if (mat == null)
+            {
+                mat = new Material(Shader.Find("TA/SSAA"));
+            }
+
+            Graphics.Blit(src, dest, mat);
+        }
+    }
+}
---
+++ 后处理/SSAA/SSAA.shader
@@ -0,0 +1,80 @@
+﻿Shader "TA/SSAA"
+{
+    Properties
+    {
+    }
+
+    SubShader
+    {
+        Tags
+        {
+            "RenderType"="Opaque"
+        }
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D_float SSAART;
+            // float4 SSAART_TexelSize;
+            float4 SSAART_TexelSize;
+
+            v2f vert(appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            float4 frag(v2f i) : SV_Target
+            {
+                float4 pixel = 0;
+                float num = 9;
+
+                float2 ops[] =
+                {
+                    float2(0,0),
+                    float2(1, 1),float2(-1, 1),float2(1, -1),float2(-1, -1),
+                    float2(0, 1),float2(0, -1),float2(1, 0),float2(-1, 0),
+                };
+
+                for (int j=0;j<num;j++)
+                {
+                    float4 weight = tex2D(SSAART, i.uv + ops[j] * SSAART_TexelSize);
+                    pixel += weight;
+                }
+
+                pixel /= num;
+
+                return pixel;
+                
+                //
+                //
+                // float4 c0 = tex2D(SSAART, i.uv + float2(1, 1) * SSAART_TexelSize);
+                // float4 c1 = tex2D(SSAART, i.uv + float2(-1, 1) * SSAART_TexelSize);
+                // float4 c2 = tex2D(SSAART, i.uv + float2(1, -1) * SSAART_TexelSize);
+                // float4 c3 = tex2D(SSAART, i.uv + float2(-1, -1) * SSAART_TexelSize);
+                // cc = (cc + c0 + c1 + c2 + c3) * 0.2;
+            }
+
+            ENDCG
+        }
+    }
+}
